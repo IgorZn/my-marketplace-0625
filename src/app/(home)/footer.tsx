2d83aa3d88a67c07864ca99b464565ba5346eb5c
igor.znamensky <igor.znamensky@gmail.com>
2025-07-07 10:23:53 +0300

    feat(ui): +navbar done #DEV-1234

---
+++ src/app/(home)/footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className={'border-t flex justify-center font-medium p-6'}>
+      <div className={'flex gap-2 items-center'}>
+        <p>funroad, Inc</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '@/app/(home)/navbar';
+import Footer from '@/app/(home)/footer';
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      {children}
+      <div className={'flex-1 bg-[#f4f4f0]'}>{children}</div>
+      <Footer />
     </div>
   );
 }
